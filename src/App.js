afb20d715a7be19e4a9b17de89b902747892c8ee
soondubusoup <84359392+soondubusoup@users.noreply.github.com>
2022-05-24 20:55:16 -0500

    shop almost open

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import './App.css';
-
-function App() {
+const App = () =>  {
   return (
-    <div>
-      Hello 86
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img />  */}
+        <div classname ="category-body-container">
+          <h2>Cars</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img />  */}
+        <div classname ="category-body-container">
+          <h2>Wheels</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img />  */}
+        <div classname ="category-body-container">
+          <h2>Aerodynamics</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img />  */}
+        <div classname ="category-body-container">
+          <h2>Bolt On</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img />  */}
+        <div classname ="category-body-container">
+          <h2>Accessories</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
     </div>
   );
 }
